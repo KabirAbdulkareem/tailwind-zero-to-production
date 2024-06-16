fd4ffa7339a3707f90d6ba6a5c293872c2c03769
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2024-06-16 08:22:33 +0100

    Made changes to some file so tat they work well as a react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 export default function App() {
   return (
-    <div className="lg:grid lg:grid-cols-2 2xl:grid-cols-5">
+    <div class="lg:grid lg:grid-cols-2 2xl:grid-cols-5">
 
-    <div className="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 2xl:col-span-2">
+    <div class="px-8 py-12 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 2xl:col-span-2">
 
-      <div className="xl:max-w-xl"> <img className="h-10" src="/img/logo.svg" alt="workcation" />
-        <img className="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover object-center lg:hidden"
+      <div class="xl:max-w-xl"> <img class="h-10" src="/img/logo.svg" alt="workcation" />
+        <img class="mt-6 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:w-full sm:object-cover object-center lg:hidden"
           src="/img/beach-work.jpg" alt="woman workcationing at the beach" />
-        <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">You can work from
+        <h1 class="mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">You can work from
           anywhere.
           <br />
-          <span className="text-indigo-500">Take
+          <span class="text-indigo-500">Take
             advantage of it.</span>
         </h1>
-        <p className="mt-2 text-gray-600 sm:text-xl">Workation helps you find work-friendly rentals in beautiful locations
+        <p class="mt-2 text-gray-600 sm:text-xl">Workation helps you find work-friendly rentals in beautiful locations
           so
           you can enjoy
           some nice wether even when you're not on vacation.</p>
-        <div className="mt-4 space-x-1 sm:mt-6">
-          <a href="#" className="btn btn-primary shadow-lg hover:-translate-y-0.5 transform transition">Book
+        <div class="mt-4 space-x-1 sm:mt-6">
+          <a href="#" class="btn btn-primary shadow-lg hover:-translate-y-0.5 transform transition">Book
             your escape</a>
 
-          <a href="#" className="btn btn-secondary">Book
+          <a href="#" class="btn btn-secondary">Book
             your escape</a>
         </div>
       </div>
@@ -30,8 +30,8 @@ export default function App() {
     </div>
 
 
-    <div className="hidden relative lg:block 2xl:col-span-3">
-      <img className="absolute inset-0 w-full h-full object-cover object-center" src="/img/beach-work.jpg"
+    <div class="hidden relative lg:block 2xl:col-span-3">
+      <img class="absolute inset-0 w-full h-full object-cover object-center" src="/img/beach-work.jpg"
         alt="woman workcationing at the beach" />
     </div>
 
