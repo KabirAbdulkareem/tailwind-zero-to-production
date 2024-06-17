a554e1a2907398ce6582c7af4ece1922c93137d7
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2024-06-17 17:45:36 +0100

    Implemented the Cards and the responsivness for the card section on various screens

--- src/App.jsx
+++ src/App.jsx
@@ -28,10 +28,6 @@ export default function App() {
               >
                 Book your escape
               </a>
-
-              <a href="#" class="btn btn-secondary">
-                Book your escape
-              </a>
             </div>
           </div>
         </div>
@@ -47,7 +43,118 @@ export default function App() {
 
       <div className="max-w-md sm:max-w-xl lg:max-w-6xl mx-auto px-8 lg:px-12 py-8">
         <h1 className="text-xl text-gray-900">Polpular destinations</h1>
-        <p className="mt-2 text-gray-600">A selection of great work-friendly cities with lots to see and explore.</p>
+        <p className="mt-2 text-gray-600">
+          A selection of great work-friendly cities with lots to see and
+          explore.
+        </p>
+
+        <div id="cards" className="mt-6 grid gap-6 lg:grid-cols-2 xl:grid-cols-3">
+
+          <div id="card" className="flex items-center bg-white rounded-lg shadow-lg overflow-hidden">
+            <img
+              className="h-32 w-32"
+              src="/img/toronto.jpg"
+              alt="Toronto skyline"
+            />
+            <div className="px-6 py-4">
+              <h3 className="text-lg font-semibold text-gray-800">Toronto</h3>
+              <p className="text-gray-600">$120 / night average</p>
+              <div className="mt-4">
+                <a href="#" className="text-indigo-500 hover:text-indigo-400">
+                  Explore 76 properties
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div id="card" className="flex items-center bg-white rounded-lg shadow-lg overflow-hidden">
+            <img
+              className="h-32 w-32"
+              src="/img/malibu.jpg"
+              alt="Malibu clifs"
+            />
+            <div className="px-6 py-4">
+              <h3 className="text-lg font-semibold text-gray-800">Malibu</h3>
+              <p className="text-gray-600">$120 / night average</p>
+              <div className="mt-4">
+                <a href="#" className="text-indigo-500 hover:text-indigo-400">
+                  Explore 76 properties
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div id="card" className="flex items-center bg-white rounded-lg shadow-lg overflow-hidden">
+            <img
+              className="h-32 w-32"
+              src="/img/chicago.jpg"
+              alt="Chicago city"
+            />
+            <div className="px-6 py-4">
+              <h3 className="text-lg font-semibold text-gray-800">Chicago</h3>
+              <p className="text-gray-600">$120 / night average</p>
+              <div className="mt-4">
+                <a href="#" className="text-indigo-500 hover:text-indigo-400">
+                  Explore 76 properties
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div id="card" className="flex items-center bg-white rounded-lg shadow-lg overflow-hidden">
+            <img
+              className="h-32 w-32"
+              src="/img/seattle.jpg"
+              alt="Toronto skyline"
+            />
+            <div className="px-6 py-4">
+              <h3 className="text-lg font-semibold text-gray-800">Seattle</h3>
+              <p className="text-gray-600">$120 / night average</p>
+              <div className="mt-4">
+                <a href="#" className="text-indigo-500 hover:text-indigo-400">
+                  Explore 76 properties
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div id="card" className="flex items-center bg-white rounded-lg shadow-lg overflow-hidden">
+            <img
+              className="h-32 w-32"
+              src="/img/colorado.jpg"
+              alt="Toronto skyline"
+            />
+            <div className="px-6 py-4">
+              <h3 className="text-lg font-semibold text-gray-800">Colorado</h3>
+              <p className="text-gray-600">$120 / night average</p>
+              <div className="mt-4">
+                <a href="#" className="text-indigo-500 hover:text-indigo-400">
+                  Explore 76 properties
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div id="card" className="flex items-center bg-white rounded-lg shadow-lg overflow-hidden">
+            <img
+              className="h-32 w-32"
+              src="/img/miami.jpg"
+              alt="Toronto skyline"
+            />
+            <div className="px-6 py-4">
+              <h3 className="text-lg font-semibold text-gray-800">Miami</h3>
+              <p className="text-gray-600">$120 / night average</p>
+              <div className="mt-4">
+                <a href="#" className="text-indigo-500 hover:text-indigo-400">
+                  Explore 76 properties
+                </a>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+        
       </div>
     </div>
   );
