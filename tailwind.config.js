25c9097f63a01ff2ecb7547f41d30b881908fb8b
Kabir Abdulkareem <kabirabdulkareem@gmail.com>
2024-06-19 07:52:32 +0100

    Customised tailwind to use Poppins font for the heading of the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         brand: {
-          light:"#3fbaeb",
-          DEFAULT:"#0fa9e6",
-          dark:"#0c87b8"
+          light: "#3fbaeb",
+          DEFAULT: "#0fa9e6",
+          dark: "#0c87b8",
         },
+      },
+      fontFamily: {
+      'headline': 'Poppins, sans-serif',
       },
     },
   },
   plugins: [],
-}
+};
